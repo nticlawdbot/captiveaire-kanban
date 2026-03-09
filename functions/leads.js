0d0ef27fda347d65dc565bf9c1f51e9092f6bced
nticlawdbot <nticlawdbot@captiveaire.dev>
2026-03-09 15:06:06 -0600

    Cache bust: Force Netlify redeploy - Smartsheet leads data corrected

--- functions/leads.js
+++ functions/leads.js
@@ -1,4 +1,5 @@
 // Netlify Function: Fetch leads from Smartsheet
+// Cache bust: 2026-03-09 15:05 - Smartsheet data corrected, all leads now have Stage = Prospect
 const https = require('https');
 
 const SMARTSHEET_API_TOKEN = process.env.SMARTSHEET_API_TOKEN || '6Q82Fjy1NKLHhEufhHshTSi5gje1EfmtgXK0D';
